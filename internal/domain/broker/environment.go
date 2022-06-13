7c3e621449439add6a14c4e8b997700215649ff2
Everton Tomalok <evertontomalok123@gmail.com>
2022-06-13 16:57:20 -0300

    feat: Prepare compensation workers

--- internal/domain/broker/environment.go
+++ internal/domain/broker/environment.go
@@ -1,7 +1,9 @@
 package broker
 
 const (
-	OrchestatratorTopic       string = "topic-orchestrator"
-	UserBalanceValidatorTopic string = "topic-user-balance-validator"
-	UserStatusValidatorTopic  string = "topic-user-status-validation"
+	OrchestatratorTopic          string = "topic-orchestrator"
+	UserBalanceValidatorTopic    string = "topic-user-balance-validator"
+	UserBalanceCompensationTopic string = "topic-user-balance-compensation"
+	UserStatusValidatorTopic     string = "topic-user-status-validation"
+	UserStatusCompensationTopic  string = "topic-user-status-compensation"
 )
--- internal/ui/workers/validate-user-worker.go
+++ internal/ui/workers/validate-user-worker.go
@@ -14,13 +14,19 @@ import (
 
 func StartValidateUserStatus(ctx context.Context, config app.Config) {
 	router := kafkaAdapter.NewRouter()
-	subscriber := kafkaAdapter.NewSubscriber("validate-user-status-consumer", config.Kafka.Host, config.Kafka.Port)
+	subscriber := kafkaAdapter.NewSubscriber("user-status-consumer", config.Kafka.Host, config.Kafka.Port)
 
 	router.AddNoPublisherHandler(
 		"validate-user-status-orders",
 		broker.UserStatusValidatorTopic,
 		subscriber,
 		validateBalanceOrder,
+	)
+	router.AddNoPublisherHandler(
+		"compensate-user-status-orders",
+		broker.UserStatusCompensationTopic,
+		subscriber,
+		rowBackUserStatusOrder,
 	)
 	done := utils.MakeDoneSignal()
 	go func() {
@@ -34,6 +40,13 @@ func StartValidateUserStatus(ctx context.Context, config app.Config) {
 }
 
 func validateUserStatusOrder(msg *message.Message) error {
+	log.Printf("received message: %s, payload: %s", msg.UUID, string(msg.Payload))
+	internalMessage, metadata, err := broker.ParseBrokerInternalMessage(msg)
+	log.Printf("%+v | %+v | %+v \n\n", internalMessage, metadata, err)
+	return nil
+}
+
+func rowBackUserStatusOrder(msg *message.Message) error {
 	log.Printf("received message: %s, payload: %s", msg.UUID, string(msg.Payload))
 	internalMessage, metadata, err := broker.ParseBrokerInternalMessage(msg)
 	log.Printf("%+v | %+v | %+v \n\n", internalMessage, metadata, err)
